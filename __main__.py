e9310f749f5b306b051a2d86e4ee310cb09e39a0
Ritika-0909 <ritikarani0520@gmail.com>
2026-07-11 15:13:34 +0530

    Add files via upload

--- __main__.py
+++ __main__.py
@@ -1,4 +1,12 @@
-from .distro import main
+import argparse
 
-if __name__ == "__main__":
-    main()
+from pip._vendor.certifi import contents, where
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--contents", action="store_true")
+args = parser.parse_args()
+
+if args.contents:
+    print(contents())
+else:
+    print(where())
